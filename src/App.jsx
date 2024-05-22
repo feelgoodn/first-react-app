54d1ac102483ea8a4091a575f8e28535aafd9801
feelgoodn <nikhilkotale2000iit@gmail.com>
2024-05-22 08:26:49 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <header>
+        <h1>Welcome to my React App</h1>
+      </header>
+      <main>
+        <p>This is the main content of the app.</p>
+      </main>
+      <footer>
+        <p>&copy; 2023 My React App</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
